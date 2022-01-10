7e4a5874b7145a5857fe880c012264264b5b08ce
Sudhanshu Randive <s37randive@gmail.com>
2022-01-10 21:19:13 +0530

    succesfully integrated an animated register page :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import Home from './pages/home/Home'
-import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
 
 function App() {
   return (

d674e8a7ff18b81481bb95a0393afcbc106e6da6
Sudhanshu Randive <s37randive@gmail.com>
2022-01-24 18:10:20 +0530

    finalised login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import './App.scss';
 import Home from './pages/home/Home'
 import Watch from './pages/watch/Watch';
 import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div>
       {/* <Home></Home> */}
       {/* <Watch></Watch> */}
-      <Register></Register>
+      {/* <Register></Register> */}
+      <Login></Login>
     </div>
   );
 }
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './login.scss'
 
 const Login = () => {
     return (
         <div className='login'>
-
+            <div className='login__top'>
+                <div className='login__top__wrapper'>
+                    <img className='login__top__wrapper__logo' src={process.env.PUBLIC_URL + '/images/logo.png'} alt="MovieZine logo" ></img>
+                </div>
+            </div>
+            <div className='login__container'>
+                <form className='login__container__form'>
+                    <h1>Sign In</h1>
+                    <input className='login__container__form__input' type="email" placeholder='Enter email'></input>
+                    <input className='login__container__form__input' type="password" placeholder='Enter password'></input>
+                    <button className='login__container__form__loginButton'>Sign In</button>
+                    <span>New to MovieZine? <b>Sign Up Now!</b></span>
+                </form>
+            </div>
         </div>
     )
 }

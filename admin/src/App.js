eaa7ce9b28c7ec46164b3dfb9d9f69402a4901c9
Sudhanshu Randive <s37randive@gmail.com>
2022-01-31 08:58:45 +0530

    new product page

--- admin/src/App.js
+++ admin/src/App.js
@@ -8,6 +8,7 @@ import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
 import Product from './pages/product/Product';
+import NewProduct from './pages/newProduct/NewProduct';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/newUser" element={<NewUser></NewUser>} />
           <Route path="/products" element={<ProductList></ProductList>} />
           <Route path="/product/:productId" element={<Product></Product>} />
+          <Route path="/newProduct" element={<NewProduct></NewProduct>} />
         </Routes>
       </div>
     </Router>

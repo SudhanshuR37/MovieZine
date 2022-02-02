8cb8dc5b3489f577a45b0a801519469dc71e39d2
Sudhanshu Randive <s37randive@gmail.com>
2022-02-02 17:30:15 +0530

    List Context API and list of List page

--- admin/src/context/listContext/apiCalls.js
+++ admin/src/context/listContext/apiCalls.js
@@ -1,62 +1,63 @@
 import axios from "axios";
-import { getMoviesStart, getMoviesSuccess, getMoviesFailure, deleteMovieStart, deleteMovieSuccess, deleteMovieFailure, createMovieStart, createMovieSuccess, createMovieFailure, updateMovieStart, updateMovieSuccess, updateMovieFailure } from "./MovieAction"
+import { getListsStart, getListsSuccess, getListsFailure, deleteListStart, deleteListSuccess, deleteListFailure, createMovieStart, createMovieSuccess, createMovieFailure, updateMovieStart, updateMovieSuccess, updateMovieFailure } from "./ListAction"
+
 
 //get
-export const getMovies = async (dispatch) => {
-    dispatch(getMoviesStart());
+export const getLists = async (dispatch) => {
+    dispatch(getListsStart());
     try {
-        const res = await axios.get("http://localhost:8800/server/movies/find", {
+        const res = await axios.get("http://localhost:8800/server/lists", {
             headers: {
                 token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
             },
         });
-        dispatch(getMoviesSuccess((res.data)));
+        dispatch(getListsSuccess((res.data)));
     } catch (err) {
-        dispatch(getMoviesFailure());
+        dispatch(getListsFailure());
     }
 };
 
 //delete
-export const deleteMovie = async (id, dispatch) => {
-    dispatch(deleteMovieStart());
+export const deleteList = async (id, dispatch) => {
+    dispatch(deleteListStart());
     try {
-        await axios.delete("http://localhost:8800/server/movies/" + id, {
+        await axios.delete("http://localhost:8800/server/lists/" + id, {
             headers: {
                 token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
             },
         });
-        dispatch(deleteMovieSuccess(id));
+        dispatch(deleteListSuccess(id));
     } catch (err) {
-        dispatch(deleteMovieFailure());
+        dispatch(deleteListFailure());
     }
 };
 
-//create
-export const createMovie = async (movie, dispatch) => {
-    dispatch(createMovieStart());
-    try {
-        const res = await axios.post("http://localhost:8800/server/movies/", movie, {
-            headers: {
-                token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
-            },
-        });
-        dispatch(createMovieSuccess(res.data));
-    } catch (err) {
-        dispatch(createMovieFailure());
-    }
-};
+// //create
+// export const createMovie = async (movie, dispatch) => {
+//     dispatch(createMovieStart());
+//     try {
+//         const res = await axios.post("http://localhost:8800/server/movies/", movie, {
+//             headers: {
+//                 token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+//             },
+//         });
+//         dispatch(createMovieSuccess(res.data));
+//     } catch (err) {
+//         dispatch(createMovieFailure());
+//     }
+// };
 
-//update
-export const updateMovie = async (movie, dispatch) => {
-    dispatch(updateMovieStart());
-    try {
-        const res = await axios.put("http://localhost:8800/server/movies/" + movie._id, movie, {
-            headers: {
-                token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
-            },
-        });
-        dispatch(updateMovieSuccess(res.data));
-    } catch (err) {
-        dispatch(updateMovieFailure());
-    }
-};
+// //update
+// export const updateMovie = async (movie, dispatch) => {
+//     dispatch(updateMovieStart());
+//     try {
+//         const res = await axios.put("http://localhost:8800/server/movies/" + movie._id, movie, {
+//             headers: {
+//                 token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+//             },
+//         });
+//         dispatch(updateMovieSuccess(res.data));
+//     } catch (err) {
+//         dispatch(updateMovieFailure());
+//     }
+// };
--- admin/src/pages/listList/ListList.jsx
+++ admin/src/pages/listList/ListList.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
-import './listList.css'
+import "./listList.css";
+import { DataGrid } from '@mui/x-data-grid';
+import { DeleteOutline } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { ListContext } from "../../context/listContext/ListContext";
+import { getLists, deleteList } from "../../context/listContext/apiCalls";
 
-const ListList = () => {
-    return <div>
-asdsad
-    </div>;
-};
+export default function ListList() {
+    const { lists, dispatch } = useContext(ListContext);
 
-export default ListList;
+    useEffect(() => {
+        getLists(dispatch);
+    }, [dispatch])
+
+    const handleDelete = (id) => {
+        deleteList(id, dispatch);
+    };
+    const columns = [
+        { field: "_id", headerName: "ID", width: 250 },
+        { field: "title", headerName: "Title", width: 250 },
+        { field: "genre", headerName: "Genre", width: 150 },
+        { field: "type", headerName: "Type", width: 150 },
+        {
+            field: "action",
+            headerName: "Action",
+            width: 150,
+            renderCell: (params) => {
+                return (
+                    <>
+                        <Link to={"/product/" + params.row._id} state={params.row}>
+                            <button className="productListEdit">Edit</button>
+                        </Link>
+                        <DeleteOutline
+                            className="productListDelete"
+                            onClick={() => handleDelete(params.row._id)}
+                        />
+                    </>
+                );
+            },
+        },
+    ];
+
+    return (
+        <div className="productList">
+            <DataGrid rows={lists} disableSelectionOnClick columns={columns} pageSize={9} checkboxSelection getRowId={(r) => r._id}></DataGrid>
+        </div>
+    );
+}
